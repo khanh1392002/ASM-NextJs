0bca9089c8dc1e78b5a6fe1b965ccfb8a37e711e
khanhnq <nqk1392002hg@gmail.com>
2022-07-30 14:14:08 +0700

    update

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -2,21 +2,24 @@ import React from 'react'
 
 type Props = {}
 
-const Signin = (props: Props) => {
+const Signup = (props: Props) => {
   return (
     <div className={`bg-fuchsia-600 h-screen relative`}>
-        <div className={`bg-white absolute left-1/3 top-48 text-center`}>
+        <div className={`bg-white absolute left-1/3 top-36 text-center`}>
         <h1 className={`text-6xl py-8 px-8 `}>Login Form</h1>
         <div className={`auth flex justify-center pb-8 px-32`}>
-            <div className={` border-slate-900 border py-3  border-solid rounded  bg-fuchsia-600`}>
-                <a href="" className={` text-white px-16 no-underline font-bold`} >Login</a>
+            <div className={` border-slate-900 border py-3  border-solid rounded `}>
+                <a href="/signin" className={`  px-16 no-underline `} >Login</a>
             </div>
-            <div className={` border-slate-900 border  py-3 border-solid rounded `}>
-                <a href=""  className={` px-16 no-underline`}>Signup</a>
+            <div className={` border-slate-900 border  py-3 border-solid rounded  bg-fuchsia-600`}>
+                <a href=""  className={` px-16 no-underline text-white font-bold` }>Signup</a>
             </div>
         </div>
         <div className="form">
             <form action="">
+                <div >
+                    <input className={`p-4  my-4 border-slate-900 border w-96 border-solid rounded`} type="text" placeholder='Name'  />
+                </div>
                 <div >
                     <input className={`p-4  my-4 border-slate-900 border w-96 border-solid rounded`} type="email" placeholder='Email Address'  />
                 </div>
@@ -27,16 +30,16 @@ const Signin = (props: Props) => {
                     <a href="">Forgot Password</a>
                 </div>
                 <div>
-                    <button className={ ` my-4 border-slate-900 border py-4 w-96 border-solid px-16 font-bold rounded text-white bg-fuchsia-600`}>Login</button>
+                    <button className={ ` my-4 border-slate-900 border py-4 w-96 border-solid px-16 font-bold rounded text-white bg-fuchsia-600`}>Signup</button>
                 </div>
             </form>
         </div>
         <div className={`my-8`}>
-            <span>Not a member?</span><a href="" className={`text-purple-600`}>Signup Now</a>
+            <a href="" className={`text-purple-600`}>Signin Now</a>
         </div>
         </div>
     </div>
   )
 }
 
-export default Signin
+export default Signup
