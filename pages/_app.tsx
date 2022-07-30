3abb8b8c2f7eb4754ec3ced81b7c66fc79fb351f
khanhnq <nqk1392002hg@gmail.com>
2022-07-30 14:30:28 +0700

    fetch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,9 @@
 import '../styles/globals.css'
-<<<<<<< HEAD
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
 
-
-=======
-import { AppPropsWithLayout } from '../model/layout'
-import Layout from '../components/Layout';
-import { AppProps } from 'next/app';
-import '../styles/style.css'
->>>>>>> 3fb43f09800e469513db02df6710042fb28102db
 function MyApp({ Component, pageProps }: AppProps) {
-
- const LayoutWrapper = Component.Layout ?? Layout;
-  
-  return (<LayoutWrapper><Component {...pageProps} /></LayoutWrapper>)
+  return <Component {...pageProps} />
 }
 
 export default MyApp
