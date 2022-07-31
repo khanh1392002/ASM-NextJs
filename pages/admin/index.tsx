3718a0bd9bbfb0d36305c18bd8fa948a9a07ffb7
khanhnq <nqk1392002hg@gmail.com>
2022-07-31 11:48:58 +0700

    abc

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -3,41 +3,37 @@ import React from 'react'
 type Props = {}
 
 const admin = (props: Props) => {
-  return (
-    <div><div id="wrapper">
-    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-        <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-            <div className="sidebar-brand-icon rotate-n-15">
-                <i className="fas fa-laugh-wink"></i>
+    return (
+        <div>
+            <div id="wrapper">
+                <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+                    <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <div className="sidebar-brand-icon rotate-n-15">
+                            <i className="fas fa-laugh-wink"></i>
+                        </div>
+                        <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                    </a>
+                    <li className="nav-item active">
+                        <a className="nav-link" href="index.html">
+                            <span>Dashboard</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                            aria-expanded="true" aria-controls="collapseTwo">
+                            <i className="fas fa-fw fa-cog"></i>
+                            <span>List</span>
+                        </a>
+                        <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                            <div className="bg-white py-2 collapse-inner rounded">
+                                <a className="collapse-item" href="">Product</a>
+                                <a className="collapse-item" href="">Category</a>
+                                <a className="collapse-item" href="">User</a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-            <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
-        </a>
-        <li className="nav-item active">
-            <a className="nav-link" href="index.html">
-                
-                <span>Dashboard</span></a>
-        </li>
-
-        <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                aria-expanded="true" aria-controls="collapseTwo">
-                <i className="fas fa-fw fa-cog"></i>
-                <span>List</span>
-            </a>
-            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div className="bg-white py-2 collapse-inner rounded">
-                    
-                    <a className="collapse-item" href="">Product</a>
-                    <a className="collapse-item" href="">Category</a>
-                    <a className="collapse-item" href="">User</a>
-                    
-                </div>
-            </div>
-        </li>
-    </ul>
-  
-</div></div>
-  )
+        </div>
+    )
 }
-
-export default  admin
+export default admin
