ed61a8429dcd929470a9abc0b418f2f6ceca47a6
giangnqph15195 <giangqu13579@gmail.com>
2022-08-02 14:54:24 +0700

    update bug css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/style.css'
 import Layout from '../components/Layout';
 function MyApp({ Component, pageProps }: AppProps) {
   const LayoutWrapper = Component.Layout ?? Layout;
