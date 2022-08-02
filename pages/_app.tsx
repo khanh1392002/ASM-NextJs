644c6c8ec9e161d046e95487da3657f68f791f2c
giangnqph15195 <giangqu13579@gmail.com>
2022-08-02 13:47:14 +0700

    bug layout website

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.css'
-
+import Layout from '../components/Layout';
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const LayoutWrapper = Component.Layout ?? Layout;
+  
+  return (<LayoutWrapper><Component {...pageProps} /></LayoutWrapper>)
+  // return <Component {...pageProps} />
+
 }
 
 export default MyApp
