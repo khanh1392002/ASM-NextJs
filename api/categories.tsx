ac9b14f78c689b29df23decae9fa8591c2d92fea
giangnqph15195 <giangqu13579@gmail.com>
2022-08-03 16:43:50 +0700

    update Edit Category Admin

--- api/categories.tsx
+++ api/categories.tsx
@@ -4,7 +4,12 @@ import instance from "./config"
 export const getall = (url:string) =>{
     return instance.get(url)
 }
-
+export const getone = (id:string) =>{
+    return instance.get(`/categorys/${id}`)
+}
 export const addct = (cate:CateType) =>{
     return instance.post('/categorys',cate)
+}
+export const updatecate = (cate:CateType) =>{
+    return instance.put(`/category/${cate._id}/edit`,cate)
 }
---
+++ type/products.tsx
@@ -0,0 +1,8 @@
+export type Iproduct = {
+    _id?:number,
+    name:string,
+    image:string,
+    price:number,
+    description: string,
+    category: string
+}
