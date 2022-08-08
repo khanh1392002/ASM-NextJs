2713a316ad528fa5b41a279a7f0f01f77fcc70e9
khanh1392002 <nqk1392002hg@gmail.com>
2022-08-08 20:54:32 +0700

    update validate form

--- pages/admin/categories/edit/[id].tsx
+++ pages/admin/categories/edit/[id].tsx
@@ -40,7 +40,13 @@ const editcategory = ({category}: ProductProps) => {
                 <form onSubmit={handleSubmit(onSubmit)} action="" className='w-[350px] m-auto border p-[30px]'>
                 <div className="col-span-6 sm:col-span-3">
                     <label  className="block text-sm font-medium text-gray-700">Name Category:</label>
-                    <input  type="text" id="first-name" autoComplete='given-name' className="mt-4 p-[10px] focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" {...register('name')} />
+                    <input  type="text" id="first-name" autoComplete='given-name' className="mt-4 p-[10px] focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" 
+                    {...register('name',{required: true})} />
+                    {Object.keys(errors).length !== 0 && (
+                      <ul>
+                        {errors.name?.type == 'required' && (<li className='text-[red] p-[15px]'>Không được để trống</li>)}
+                      </ul>
+                    )}
               </div>
 
               <button type="submit" className="mt-[15px] inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
